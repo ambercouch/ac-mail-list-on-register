c61e0d264253c139e177c302f926a2abb480a1e0
Richie Arnold <richard@ambercouch.co.uk>
2021-07-04 15:00:40 +0100

    First Commit

--- ac-sendinblue-on-register.php
+++ ac-sendinblue-on-register.php
@@ -26,7 +26,12 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+
 defined('ABSPATH') or die('You do not have the required permissions');
+
+require_once(__DIR__ . '/vendor/autoload.php');
+
+define('ACSIBR_KEY', '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');
 
 // Define path and URL to the ACF plugin.
 //define( 'MY_ACF_PATH', 'inc/acf/' );
@@ -44,7 +49,7 @@ defined('ABSPATH') or die('You do not have the required permissions');
 // Customize the url setting to fix incorrect asset URLs.
 //add_filter('acf/settings/url', 'my_acf_settings_url');
 function my_acf_settings_url( $url ) {
-   // return MY_ACF_URL;
+    // return MY_ACF_URL;
 }
 
 // (Optional) Hide the ACF admin menu item.
@@ -55,6 +60,12 @@ function my_acf_settings_show_admin( $show_admin ) {
 
 // Include the plugin-code.
 require_once(  'lib/acsibr.php' );
+
+
+
+
+
+
 
 
 
--- lib/acsibr.php
+++ lib/acsibr.php
@@ -1,26 +1,46 @@
 <?php
 
-function acsibr() {
 
-   if(isset($_POST['register_terms_agree'])){
-       echo "I've read the terms";
-       die();
-   }
 
+
+add_action( 'user_register', 'acsibr_user_register', 10, 1 );
+
+function acsibr_user_register($user_id)
+{
+    $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', ACSIBR_KEY);
+
+if(isset($_POST['register_terms_agree']))
+{
+
+
+    $apiInstance = new SendinBlue\Client\Api\ContactsApi(new GuzzleHttp\Client(), $credentials);
+
+    $createContact = new \SendinBlue\Client\Model\CreateContact([
+        'email' => $_POST['register_email'],
+        'updateEnabled' => true,
+        'attributes' => (object)[
+            'FIRSTNAME' => $_POST['register_first_name'],
+            'LASTNAME' => $_POST['register_last_name'],
+            'isVIP' => 'true'
+        ],
+        'listIds' => [
+            2,
+            5
+        ]
+    ]);
+
+    try
+    {
+        $result = $apiInstance->createContact($createContact);
+        print_r($result);
+    } catch (Exception $e)
+    {
+        echo "<pre>";
+        print_r($e);
+        echo 'Exception when calling ContactsApi->createContact: ', $e->getMessage(), PHP_EOL;
+        echo "</pre>";
+    }
+}else{
+    return;
 }
-
-add_action( 'user_register', 'acsibr', 10, 1 );
-
-
-function acsm_enqueue_scripts(){
-    wp_register_script('acsibr_script', plugins_url('../assets/js/scripts-acsibr.js', __FILE__), array('jquery'), '1.1', true);
-    wp_enqueue_script('acsibr_script');
 }
-
-//add_action( 'wp_enqueue_scripts', 'acsm_enqueue_scripts' );
-
-function acsm_enqueue_styles() {
-    wp_register_style('acsibr_styles', plugins_url('../assets/css/styles-acsibr.css', __FILE__));
-    wp_enqueue_style('acsibr_styles');
-}
-//add_action( 'get_footer', 'acsm_enqueue_styles' );

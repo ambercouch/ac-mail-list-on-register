bdcb025850330b3acdc10b0d0edcec2e5ad28873
Richard Arnold <richard@ambercouch.co.uk>
2021-07-09 11:14:03 +0100

    Update plugin name and references from sendinblue to emal list

--- ac-email-list-on-register.php
+++ ac-email-list-on-register.php
@@ -1,13 +1,13 @@
 <?php
 /*
-  Plugin Name: AC Sendinblue on register
+  Plugin Name: AC email list on register
   Plugin URI: https://github.com/ambercouch/ac-wp-simple-modal
   Description: Add your user to Sendinblue when they register
   Version: 0.1.0
   Author: AmberCouch
   Author URI: http://ambercouch.co.uk
   Author Email: richard@ambercouch.co.uk
-  Text Domain: acsibr
+  Text Domain: acelr
   Domain Path: /lang/
   License:
   Copyright 2018 AmberCouch
@@ -32,6 +32,7 @@ defined('ABSPATH') or die('You do not have the required permissions');
 require_once(__DIR__ . '/vendor/autoload.php');
 
 define('ACSIBR_KEY', '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');
+define( 'AC_EMAIL_LIST_ON_REGISTRATION_PLUGIN_VERSION', '1.0.0' );
 
 // Define path and URL to the ACF plugin.
 //define( 'MY_ACF_PATH', 'inc/acf/' );
@@ -59,7 +60,7 @@ function my_acf_settings_show_admin( $show_admin ) {
 }
 
 // Include the plugin-code.
-require_once(  'lib/acsibr.php' );
+require_once(  'lib/acelr.php' );
 
 
 
--- lib/acelr.php
+++ lib/acelr.php
@@ -5,8 +5,8 @@
  * Add fields to the registration form
  */
 
-add_action( 'register_form', 'acsibr_register_form' );
-function acsibr_register_form() {
+add_action( 'register_form', 'acelr_register_form' );
+function acelr_register_form() {
 
     $first_name = ( ! empty( $_POST['first_name'] ) ) ? trim( $_POST['first_name'] ) : '';
     $last_name = ( ! empty( $_POST['last_name'] ) ) ? trim( $_POST['last_name'] ) : '';
@@ -14,12 +14,12 @@ function acsibr_register_form() {
     ?>
 
     <p>
-        <label for="first_name"><?php _e( 'First Name', 'mydomain' ) ?><br />
+        <label for="first_name"><?php _e( 'First Name', 'acelr' ) ?><br />
             <input type="text" name="first_name" id="first_name" class="input" value="<?php echo esc_attr( wp_unslash( $first_name ) ); ?>" size="25" /></label>
     </p>
 
     <p>
-        <label for="last_name"><?php _e( 'Last Name', 'mydomain' ) ?><br />
+        <label for="last_name"><?php _e( 'Last Name', 'acelr' ) ?><br />
             <input type="text" name="last_name" id="last_name" class="input" value="<?php echo esc_attr( wp_unslash( $last_name ) ); ?>" size="25" /></label>
     </p>
 
@@ -27,64 +27,58 @@ function acsibr_register_form() {
 }
 
 
-add_filter( 'registration_errors', 'myplugin_registration_errors', 10, 3 );
-function myplugin_registration_errors( $errors, $sanitized_user_login, $user_email ) {
+add_filter( 'registration_errors', 'acelr_registration_errors', 10, 3 );
+function acelr_registration_errors( $errors, $sanitized_user_login, $user_email ) {
 
     if ( empty( $_POST['first_name'] ) || ! empty( $_POST['first_name'] ) && trim( $_POST['first_name'] ) == '' ) {
-        $errors->add( 'first_name_error', __( '<strong>ERROR</strong>: You must include a first name.', 'mydomain' ) );
+        $errors->add( 'first_name_error', __( '<strong>ERROR</strong>: You must include a first name.', 'acelr' ) );
     }
-    if ( empty( $_POST['last_name'] ) || ! empty( $_POST['last_name'] ) && trim( $_POST['first_name'] ) == '' ) {
-        $errors->add( 'last_name_error', __( '<strong>ERROR</strong>: You must include a first name.', 'mydomain' ) );
+    if ( empty( $_POST['last_name'] ) || ! empty( $_POST['last_name'] ) && trim( $_POST['last_name'] ) == '' ) {
+        $errors->add( 'last_name_error', __( '<strong>ERROR</strong>: You must include a last name.', 'acelr' ) );
     }
     return $errors;
 }
 
 
-add_action( 'user_register', 'myplugin_user_register' );
-function myplugin_user_register( $user_id ) {
+
+add_action( 'user_register', 'acelr_user_register', 10, 1 );
+
+function acelr_user_register($user_id)
+{
+
     if ( ! empty( $_POST['first_name'] ) ) {
         update_user_meta( $user_id, 'first_name', trim( $_POST['first_name'] ) );
         update_user_meta( $user_id, 'last_name', trim( $_POST['last_name'] ) );
     }
-}
-
-
-
-
-add_action( 'user_register', 'acsibr_user_register', 10, 1 );
-
-function acsibr_user_register($user_id)
-{
 
     $user_info = get_userdata($user_id);
 
-    $acsibr_user = [];
-    $acsibr_user['email'] = (isset($_POST['user_email'])) ? $_POST['user_email'] : $user_info->user_email;
-    $acsibr_user['first_name'] = (isset($_POST['user_first_name'])) ? $_POST['user_first_name'] : $user_info->first_name;
-    $acsibr_user['last_name'] = (isset($_POST['user_last_name'])) ? $_POST['user_last_name'] : $user_info->last_name;
-
+    $acelr_user = [];
+    $acelr_user['email'] = (isset($_POST['user_email'])) ? $_POST['user_email'] : $user_info->user_email;
+    $acelr_user['first_name'] = (isset($_POST['user_first_name'])) ? $_POST['user_first_name'] : $user_info->first_name;
+    $acelr_user['last_name'] = (isset($_POST['user_last_name'])) ? $_POST['user_last_name'] : $user_info->last_name;
 
 
     if(isset($_POST['register_terms_agree']))
     {
-        acmlr_send_to_list($acsibr_user);
+        acmlr_send_to_list($acelr_user);
     }else{
-        acmlr_send_to_list($acsibr_user);
+        acmlr_send_to_list($acelr_user);
     }
     }
 
-function acmlr_send_to_list($acsibr_user){
+function acmlr_send_to_list($acelr_user){
 
     $credentials = SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', ACSIBR_KEY);
 
     $apiInstance = new SendinBlue\Client\Api\ContactsApi(new GuzzleHttp\Client(), $credentials);
 
     $createContact = new \SendinBlue\Client\Model\CreateContact([
-        'email' => $acsibr_user['email'],
+        'email' => $acelr_user['email'],
         'updateEnabled' => true,
         'attributes' => (object)[
-            'FIRSTNAME' => $acsibr_user['first_name'],
-            'LASTNAME' => $acsibr_user['last_name'],
+            'FIRSTNAME' => $acelr_user['first_name'],
+            'LASTNAME' => $acelr_user['last_name'],
             'isVIP' => 'true'
         ],
         'listIds' => [
